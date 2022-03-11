7ad6c08874a2c66c6eba194248793da5cbc7fd5a
avenable11 <avenable11@ivytech.edu>
2022-03-10 20:38:10 -0500

    Fibonacci Sequence

--- 3_10_22/main.cpp
+++ 3_10_22/main.cpp
@@ -9,10 +9,46 @@
 using namespace std;
 
 int getInput(string);
-
+int fibNum(int, int, int, int[]);
 int main()
 {
-	ifstream in;
+	int firstNum, secondNum;
+	int current;
+	int counter;
+	int nthFib;
+	int * sequence;
+	
+	firstNum = getInput("Enter the first Fibonacci number: ");
+	secondNum = getInput("Enter the second Fibonacci number: ");
+	
+	nthFib = getInput("Enter the position of the desired Fibonacci number: ");
+	
+	sequence = new int[nthFib];
+	sequence[0] = firstNum;
+	sequence[1] = secondNum;
+	for (int i = 2; i < nthFib; i++)
+	{
+		sequence[i] = 0;
+	}
+
+	/*if (nthFib == 1)
+		current = firstNum;
+	else if (nthFib == 2)
+		current = secondNum;
+	else
+	{
+		counter = 3;
+		while (counter <= nthFib)
+		{
+			current = firstNum + secondNum;
+			firstNum = secondNum;
+			secondNum = current;
+			counter++;
+		}
+	} */
+	
+	cout << "The " << nthFib << "th Fibonacci number is " << fibNum(firstNum, secondNum, nthFib, sequence) << endl;
+	/*ifstream in;
 	in.open("input.txt");
 	
 	int size = 1000;
@@ -40,8 +76,27 @@ int main()
 	else
 		cout << "Did not find " << searchItem << endl;
 	
-	in.close();
+	in.close();*/
+	delete[] sequence;
 	return 0;
+}
+
+int fibNum(int first, int second, int n, int seq[])
+{
+	if(n == 1)
+		return first;
+	else if (n == 2)
+		return second;
+	else
+	{
+		if (seq[n-1] == 0)
+		{
+			seq[n - 1] = fibNum(first, second, n - 1, seq) + fibNum(first, second, n - 2, seq);
+		}
+		 
+			
+		return seq[n - 1];
+	}
 }
 
 int getInput(string prompt)
